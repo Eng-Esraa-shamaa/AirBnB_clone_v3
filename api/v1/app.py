e1d19b30808d51f806445c36d6eee33310224707
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-12-01 19:32:58 +0200

    .

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
-
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.teardown_appcontext
 def tear_down(self):
