4ce64c3ba4e617e25dbf2b6386828dc25e9aae06
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-12-01 17:56:56 +0200

    .

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
-'''Flask server app var'''
+"""
+flask app
+"""
 from models import storage
 from os import getenv
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from flask_cors import CORS
 
-# Create a variable app, instance from flask
 app = Flask(__name__)
-# Managing CORS (Cross Origin Resource Sharing)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-# For a better identation of JSON response
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-# register the blueprint app_views to your app
 app.register_blueprint(app_views)
-# disables this strict trailing slash behavior.
 app.url_map.strict_slashes = False
 
-
-# method to handle the close of the app
 @app.teardown_appcontext
 def down_method(self):
-    """ close the storage"""
+    """ tear down method """
     storage.close()
 
-
-# Function to handle 404 error
 @app.errorhandler(404)
 def page_not_found(e):
-    """ Method to handle
-    the error page
+    """
+    404 page
     """
     return jsonify({"error": "Not found"}), 404
 
