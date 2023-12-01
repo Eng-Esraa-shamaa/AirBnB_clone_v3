878e0fbf5a51373da2c92d6c1874ff5dc4b86148
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-12-01 17:54:14 +0200

    .

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,46 @@
 #!/usr/bin/python3
-"""
-Flask App
-"""
-from flask import Flask, Blueprint, render_template, jsonify
+'''Flask server app var'''
 from models import storage
+from os import getenv
+from flask import Flask, jsonify
 from api.v1.views import app_views
-import os
+from flask_cors import CORS
 
-
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = os.getenv('HBNB_API_PORT', '5000')
-
+# Create a variable app, instance from flask
 app = Flask(__name__)
-
+# Managing CORS (Cross Origin Resource Sharing)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+# For a better identation of JSON response
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+# register the blueprint app_views to your app
 app.register_blueprint(app_views)
+# disables this strict trailing slash behavior.
+app.url_map.strict_slashes = False
 
 
+# method to handle the close of the app
 @app.teardown_appcontext
-def tear_down():
-    """closes the current SQLAlchemy Session"""
+def down_method(self):
+    """ close the storage"""
     storage.close()
 
 
+# Function to handle 404 error
 @app.errorhandler(404)
-def error_page():
-    """handles 404 errors"""
-    return (jsonify({"error": "Not Found"}))
+def page_not_found(e):
+    """ Method to handle
+    the error page
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    app.run(host=host, port=port, debug=True)
+    if getenv('HBNB_API_HOST'):
+        host = getenv('HBNB_API_HOST')
+    else:
+        host = '0.0.0.0'
+    if getenv('HBNB_API_PORT'):
+        port = getenv('HBNB_API_PORT')
+    else:
+        port = 5000
+    app.run(host=host, port=port, threaded=True)
