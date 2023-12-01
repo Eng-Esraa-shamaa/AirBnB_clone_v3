079bf7bee1780cfff07a11b247a84c1ff518f5c2
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-12-01 17:34:47 +0200

    .

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,9 +8,6 @@ from api.v1.views import app_views
 import os
 
 
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = os.getenv('HBNB_API_PORT', 5000)
-
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
@@ -29,4 +26,12 @@ def error_page():
 
 
 if __name__ == "__main__":
-    app.run(host=host, port=port, debug=True)
+    if getenv('HBNB_API_HOST'):
+        host = getenv('HBNB_API_HOST')
+    else:
+        host = '0.0.0.0'
+    if getenv('HBNB_API_PORT'):
+        port = getenv('HBNB_API_PORT')
+    else:
+        port = 5000
+    app.run(host=host, port=port, threaded=True)
